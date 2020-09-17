d03d4c94baeef2afcdd2765ff184ab0f6eac6bbd
seth mcfeeters <seth.mcfeeters@gmail.com>
2020-09-16 20:48:05 -0700

    almost done with lab 11

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 `use strict`;
 // ------------------------global variables------------------------
 var productBucket = [];
+var left = document.getElementById('left');
+var right = document.getElementById('right');
+var center = document.getElementById('center');
+var maxClick = 0;
 // var chart = new chart(ctx)
-
 // -------------------------------object constructor-----------
 function Product(name, img) {
     this.name = name;
     this.img = img;
     this.vote = 0;
     this.totalVotes = 0;
-    this.seenIt = false;
+    this.seenIt = 0;
     productBucket.push(this);
-    console.log(productBucket.length);
 }
 
 // --------------------------------new objects--------------------------
@@ -29,7 +31,7 @@ new Product('pen', 'img/assets/pen.jpg');
 new Product('pet-sweep', 'img/assets/pet-sweep.jpg');
 new Product('scissors', 'img/assets/scissors.jpg');
 new Product('shark', 'img/assets/shark.jpg');
-new Product('sweep', 'img/assets/sweep.jpg');
+new Product('sweep', 'img/assets/sweep.png');
 new Product('tauntaun', 'img/assets/tauntaun.jpg');
 new Product('unicorn', 'img/assets/unicorn.jpg');
 new Product('usb', 'img/assets/usb.gif');
@@ -38,19 +40,13 @@ new Product('wine-glass', 'img/assets/wine-glass.jpg');
 
 // -------------------------random index value funtion-----------
 function randomIndexVal() {
-    var randomizer = Math.floor(Math.random() * productBucket.length);
+    var randomizer
+    do {
+        randomizer = Math.floor(Math.random() * productBucket.length);
+    } while (currentlyShowing.includes(randomizer))
     return randomizer;
-    // var randomizer;
-    // safety = 10;
-    // // do{ 
-    //     randomizer = Math.floor(Math.random() * productBucket.length);
-    //     safety--;
-    //     console.log(randomizer);
-    //     console.log(currentlyShowing);
-
-    // } while (currentlyShowing.includes(randomizer)&& safety) 
-
 }
+
 
 
 
@@ -58,57 +54,66 @@ function randomIndexVal() {
 var currentlyShowing = [];
 
 function render() {
-    
+
     var temp1;
     var temp2;
     var temp3;
-    console.log(temp1);
-    
+
     // rolling temp1
     temp1 = randomIndexVal();
-    
+
     // checking 2nd roll against first
     do {
         temp2 = randomIndexVal();
     } while (temp2 === temp1);
-    
+
     // checking temp3 against the other two
     do {
         temp3 = randomIndexVal();
     } while (temp3 === temp1 || temp3 === temp2);
-    
- currentlyShowing = [temp1, temp2, temp3]
-    document.getElementById('left').setAttribute('src', productBucket[temp1].img);
-    document.getElementById('center').setAttribute('src', productBucket[temp2].img);
-    document.getElementById('right').setAttribute('src', productBucket[temp3].img);
+
+    currentlyShowing = [temp1, temp2, temp3]
+    left.setAttribute('src', productBucket[temp1].img);
+    center.setAttribute('src', productBucket[temp2].img);
+    right.setAttribute('src', productBucket[temp3].img);
+    productBucket[temp1].seenIt++;
+    productBucket[temp2].seenIt++;
+    productBucket[temp3].seenIt++;
 }
 
-render ();
+render();
 
-// ----------------------increment votes----------------------------------------------------
-// need to do           
 
-// ---------------------------stick 3 imjages to page-------------------
-        //    the adress of where things go
-
-// -------------------------------clicked votes -------------------------------
-function handlevote(event) {
+// -------------------------------clicked/vote increment -------------------------------
+function handleVote(event) {
     event.preventDefault();
-    var thingThatWasClicked = event.target.attributes[1].value;
-    var productId = thingThatWasClicked.src;
-}                                      
+    var clicked = event.target.attributes[1].value;
+    this.seenIt = this.seenIt + 1;
+    for (let i = 0; i < productBucket.length; i++) {
+        if (clicked === productBucket[i].img) {
+            productBucket[i].vote++;
+        }
+    }
+    if (maxClick === 4) {
+        right.removeEventListener('click', handleVote);
+        center.removeEventListener('click', handleVote);
+        left.removeEventListener('click', handleVote);
+        
+        // call function with for loop crating list of voted/viewed items
+        document.getElementById('results').innerHTML = 'voted for' + this.vote + 'times. seen' + this.seenIt + ' times.' 
+
+    }
+    maxClick = maxClick + 1;
+
+    render();
+    console.log('maxclicks', maxClick);
+}
 
 // ---------------------------event listeners/clickers------------------------
 
-// right.addEventListener(click, handleVote);
-// center.addEventListener(click, handleVote);
-// left.addEventListener(click, handleVote);
+right.addEventListener('click', handleVote);
+center.addEventListener('click', handleVote);
+left.addEventListener('click', handleVote);
 
 // ----------------------------what is currently rendered----------------------------------------------
-// var previousimg1 = left.src;
-// var previousimg2 = right.src;
-// var previousimg3 = center.src;
 
-// var newimg  = productId[randomIndexVal()].image
-
-// img
